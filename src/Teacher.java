de4b3d123d4e2fa72bdedad8ac97b576afe80b1e
Benjamin Rudderham <brudder1@unb.ca>
2020-02-29 14:27:01 -0400

    Merging

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,32 +1,20 @@
-
+import java.util.ArrayList;
 
 public class Teacher {
 	private String name;
+	private ArrayList<String> teachables;
 	
-	public Teacher(String name, String location, ArrayList<String> teachables) {
+	public Teacher(String name, ArrayList<String> teachables) {
 		this.name = name;
-		this.location = location;
 		this.teachables = teachables;
-		absences = new ArrayList<Absence>();
 	}
 	
 	public String getName() {
 		return name;
 	}
 	
-	public String getLocation() {
-		return location;
-	}
-	
 	public ArrayList<String> getTechables() {
 		return teachables;
 	}
 	
-	public ArrayList<Absence> getAbsences(){
-		return absences;
-	}
-	
-	public void addAbsence(Absence A) {
-		absences.add(A);
-	}
 }
