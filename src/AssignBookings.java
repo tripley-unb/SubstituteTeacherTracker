ffbab7b4135d139885b7fff8c0b9fa4a6c6e4e7b
Peter <p.macdonald454@gmail.com>
2020-03-01 20:40:46 -0400

    AssignBooking class reformatted so the absences list uses a sepeerate array list to index it

--- src/AssignBookings.java
+++ src/AssignBookings.java
@@ -5,18 +5,25 @@ public class AssignBookings {
 	private ArrayList<Assignment> assignments;
  	private ArrayList<Absence> absences;
 	private ArrayList<Substitute> substitutes;
+	private ArrayList<Integer> absindex;
 	private ArrayList<Integer> subindex;
-
 	
 	private Random random;
 
 	public AssignBookings(ArrayList<Absence> a, ArrayList<Substitute> s) {
 		absences = a;
 		substitutes = s;
+		
+		absindex = new ArrayList<Integer>();//initialize list that correlates to absence list index
+		for (int i=0; i<absences.size();i++) {
+			absindex.add(i);
+		}
+		
 		subindex = new ArrayList<Integer>();//initialize list that correlates to sub list index
 		for (int i=0; i<substitutes.size();i++) {
 			subindex.add(i);
 		}
+		
 		assignments = new ArrayList<Assignment>();
 		random = new Random();
 	}
@@ -25,18 +32,18 @@ public class AssignBookings {
 		
 		int randindexS;
 		int randindexA;
-		int counter = 0;
 		
 		Substitute substitute = new Substitute();
 		Absence absence = new Absence();
 		Absence booking = new Absence();
 		boolean conflict = false;
 
-		while (absences.size()!=0) {//cycle through absences at random
+		while (absindex.size()!=0) {//cycle through absences at random
 
 			//choose one of the absences at random
-			randindexA = random.nextInt(absences.size());
-			absence = absences.get(randindexA);
+			randindexA = random.nextInt(absindex.size());
+			absence = absences.get(absindex.get(randindexA));
+			
 			for (int i=0; i<substitutes.size();i++) {//re-initialize subindex list
 				subindex.add(i);
 			}
@@ -50,11 +57,8 @@ public class AssignBookings {
 				substitute = substitutes.get(subindex.get(randindexS));				
 				
 				if (substitute.getBooking().size()!=0) {//if the sub has bookings
-
 					for (int j=0; j<substitute.getBooking().size(); j++){//cycle through sub's bookings
-						
 						booking = substitute.getBooking().get(j);
-						
 						if (booking.getDay().equals(absence.getDay()) && (booking.getTime().equals(absence.getTime()))) {//if sub is booked during absence
 							conflict = true;
 						}
@@ -63,20 +67,19 @@ public class AssignBookings {
 				
 				if (conflict == false) {//if there aren't any conflicts
 					substitute.addBooking(absence);//add booking to subs list
-					
 					//add new assignment to list
 					assignments.add(new Assignment(substitute.getName(),absence.getTeacher().getName(),absence.getTime(),absence.getDay(),absence.getLocation()));
-					absences.remove(randindexA);//remove absence from absence list
-					
+					absindex.remove(randindexA);//remove absence from absence list
 					break cyclesubs;//return to cycle absences now the assignment has been made
 				}
 				
 				subindex.remove(randindexS);//remove index representing substitute from list if they can't fill absence
 			}
+			
 			if (subindex.size() == 0) {//no subs to fill absence
-				System.out.println("no subs to fill absence: "+absence);
-				assignments.add(new Assignment("none available ",absence.getTeacher().getName(),absence.getTime(),absence.getDay(),absence.getLocation()));
-				absences.remove(randindexA);//remove absence from absence list
+				System.out.println("WARNING! no subs to fill absence: "+absence);
+				assignments.add(new Assignment("NONE AVAILABLE",absence.getTeacher().getName(),absence.getTime(),absence.getDay(),absence.getLocation()));
+				absindex.remove(randindexA);//remove absence from absence list
 			}
 		}
 	}
