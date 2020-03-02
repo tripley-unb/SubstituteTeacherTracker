95d1b255d6d3d9f2720620f35a70579d88781a7f
Thomas Ripley <tripley@unb.ca>
2020-03-01 20:59:28 -0400

    Cleaned up driver

--- src/Driver.java
+++ src/Driver.java
@@ -4,26 +4,17 @@ import java.util.ArrayList;
 public class Driver {
 
 	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-			AbsenceList abslist = InputCSV.ReadAbsenceCSV("absences.csv");
-			SubList sublist = InputCSV.ReadSubCSV("substitutes.csv");
-			System.out.println(abslist);
-			System.out.println(sublist + "\n");
-			AssignBookings assignB = new AssignBookings(abslist.getList(),sublist.getList());
-			assignB.assign();
-			ArrayList<Assignment> list = new ArrayList<Assignment>();
-			list = assignB.getAssignments();
-			System.out.println(list);
-			
-			
-//			Assignment a = new Assignment("A","b","c","d","e");
-//			Assignment b = new Assignment("fd","sdf","csd","sd","ef");
-//			ArrayList<Assignment> list = new ArrayList<Assignment>();
-//			list.add(a);
-//			list.add(b);
-//			System.out.println(list);
-			
-			OutputCSV.createCSV(list);
+		AbsenceList abslist = InputCSV.ReadAbsenceCSV("absences.csv");
+		SubList sublist = InputCSV.ReadSubCSV("substitutes.csv");
+		System.out.println(abslist);
+		System.out.println(sublist + "\n");
+		AssignBookings assignB = new AssignBookings(abslist.getList(),sublist.getList());
+		assignB.assign();
+		ArrayList<Assignment> list = new ArrayList<Assignment>();
+		list = assignB.getAssignments();
+		System.out.println(list);
+		
+		OutputCSV.createCSV(list);
 	}
 
 }
